b2f476fb56e3794b8437abb462c42f58e5913422
Gopinath <133114658+Gopinath-King@users.noreply.github.com>
2023-05-11 11:54:32 +0530

    Arithmetic assignment Operators in java

---
+++ assignment.java
@@ -0,0 +1,17 @@
+// Arithmetic assignment operator = , += , -= , *= , /= , %=
+public class assignment {
+    public static void main(String args[]){
+        int a=10;
+        System.out.println(a); // a=10
+        a+=10;
+        System.out.println(a); // a=20
+        a-=10;
+        System.out.println(a); // a=10
+        a*=5;
+        System.out.println(a); // a=50
+        a/=5;
+        System.out.println(a); // a=10
+        a%=3;
+        System.out.println(a); // a=1
+    }
+}
