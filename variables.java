19b9277d317cb7c377c255161d611571793e9c1b
Gopinath <133114658+Gopinath-King@users.noreply.github.com>
2023-05-11 11:46:17 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,25 @@
+// Variables
+import java.lang.*;
+class variables {
+        public static void main(String args[]){
+        String name="Gopinath";
+        int age=18;
+        int speed=62;
+        float percent=99.5f;
+        char firstletter='G';
+        boolean student=true;
+        System.out.println("My name is             : "+name);
+        System.out.println("My age is              : "+age);
+        System.out.println("My typing speed is     : "+speed+"WPM");
+        System.out.println("My typing accuracy is  : "+percent);
+        System.out.println("First letter           : "+firstletter);
+        System.out.println("I am student           : "+student);
+        }
+}               /* Out put:
+My name is            : Gopinath
+My age is             : 18
+My typing speed is    : 62WPM
+My typing accuracy is : 99.5
+First letter          : G
+I am student          : true
+*/
