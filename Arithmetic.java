a047856cf746e2eff6c2ca0e9ee926d5b35d6e0a
Gopinath <133114658+Gopinath-King@users.noreply.github.com>
2023-05-11 11:53:02 +0530

    Arithmetic Operators in java

---
+++ Arithmetic.java
@@ -0,0 +1,18 @@
+// Arithmetic Operators in java   + , - , * , / , % .
+import java.lang.*;
+    public class Arithmetic {
+        public static void main(String args[]){
+            int a=10 ,b=20;
+            System.out.println("Addition      : "+(a+b));
+            System.out.println("Subtraction   : "+(a-b));
+            System.out.println("Multiplication: "+(a*b));
+            System.out.println("Division      : "+(a/b));
+            System.out.println("Modulus       : "+(a%b));
+        }
+    }     /*  Output:
+    Addition      : 30
+    Subtraction    : -10
+    Multiplication: 200
+    Division      : 0
+    Modulus       : 10 */
+
