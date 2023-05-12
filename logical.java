96d357e20fc64f574ce36a6e9441166f1abc2b62
Gopinath <133114658+Gopinath-King@users.noreply.github.com>
2023-05-12 07:47:02 +0530

    Logical Operators in Java

---
+++ logical.java
@@ -0,0 +1,8 @@
+// Logical Operators  && , ||
+public class logical {
+    public static void main(String args[]){
+        int a=30, b=60;
+        System.out.println(a>40 && b>40);    // false
+        System.out.println(a>40 || b>40);    // true
+    }
+}
