9d2f331ac19ae46d588e52b775ad44b8a645d9f8
Gopinath <133114658+Gopinath-King@users.noreply.github.com>
2023-05-12 07:40:28 +0530

    Bitwise Operator in Java

---
+++ bitwise.java
@@ -0,0 +1,10 @@
+// Bitwise Operators    & , | , ^ , ~
+public class bitwise {
+    public static void main(String args[]){
+        int a=15,b=35;
+        System.out.println("Bitwise AND : "+(a&b)); // 3
+        System.out.println("Bitwise OR  : "+(a|b)); // 47
+        System.out.println("Bitwise XOR : "+(a^b)); // 44
+        System.out.println("Bitwise NOT : "+(~a));  // -16
+    }
+}
