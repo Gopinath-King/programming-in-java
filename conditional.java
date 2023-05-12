11c18d3788351f4732177c1c4f53f306662385e7
Gopinath <133114658+Gopinath-King@users.noreply.github.com>
2023-05-12 07:42:02 +0530

    Conditional or Ternary Operator in Java

---
+++ conditional.java
@@ -0,0 +1,8 @@
+// Conditional or Ternary Operator  ?:
+public class conditional {
+    public static void main(String args[]){
+        int a=10, b=50;
+        int c=a<b?a:b;   // Condition ? true : false ;
+        System.out.println("Smallest Number is : "+c);
+    }     // Output : Smallest Number is : 10
+}
