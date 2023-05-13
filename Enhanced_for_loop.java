02f1969f8cb0f170310038e2f0b402649038acfd
Gopinath <133114658+Gopinath-King@users.noreply.github.com>
2023-05-13 07:54:13 +0530

    Enhanced for loop in Java

---
+++ Enhanced_for_loop.java
@@ -0,0 +1,14 @@
+// Enhanced for loop
+public class Enhanced_for_loop {
+    public static void main(String args[]) {
+        int numbs[] = {13, 29, 33, 38, 55};
+        for (int n : numbs) {
+            System.out.println(n);
+        }
+    }
+}
+         /* Output: 13
+                    29
+                    33
+                    38
+                    55       */
